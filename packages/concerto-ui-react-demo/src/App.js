2fcd92fb8c016c5acf52d0f9819cda6295fad2e7
Matt Roberts <code@rbrts.uk>
2020-02-22 21:51:32 +0000

    style(demo): Redesign the layout of the demo app
    Signed-off-by: Matt Roberts <code@rbrts.uk>

--- packages/concerto-ui-react-demo/src/App.js
+++ packages/concerto-ui-react-demo/src/App.js
@@ -16,7 +16,8 @@
 import React, { Component, Message } from 'react';
 import './App.css';
 import { ConcertoForm } from '@accordproject/concerto-ui-react';
-import { Tab } from 'semantic-ui-react';
+import { Grid, Segment, Form, Dropdown, FormDropdown } from 'semantic-ui-react';
+import TextareaAutosize from 'react-textarea-autosize';
 
 class App extends Component {
 
@@ -101,8 +102,8 @@ class App extends Component {
     this.onModelChange = this.onModelChange.bind(this);
   }
 
-  handleDeclarationSelectionChange(event) {
-    const state = {fqn: event.target.value};
+  handleDeclarationSelectionChange(e, { value }) {
+    const state = {fqn: value};
     state.json = this.getForm().generateJSON(state.fqn);
     this.setState(state);
   }
@@ -133,20 +134,6 @@ class App extends Component {
   }
 
   render() {
-    const panes = [
-      { menuItem: 'Model Editor', render: () => (<Tab.Pane>
-        <form>
-          <div className="ui form">
-            <textarea
-              value={this.state.model}
-              onChange={this.handleModelTextAreaChange.bind(this)}
-              className={'form-control Text-area'}
-              placeholder="Paste a model file"/>
-          </div>
-        </form>
-      </Tab.Pane>) },
-    ];
-
     const form = this.state.warning
       ? (<Message>
         <p>An error occured</p>
@@ -163,38 +150,57 @@ class App extends Component {
       />;
 
     return (
-      <div className="App container ui form">
+      <div className="App">
         <h2>Concerto Form Generator - Demo Client</h2>
         <p>This tool demonstrates the <em>concerto-ui</em> library to generate a form from an Accord Project Concerto model.</p>
         <p>This demo produces a ReactJS form that is styled with Semantic UI.</p>
-        <Tab panes={panes} />
-        <p>Choose a type from this dropdown to generate a form</p>
-        <div className='field'>
-          <label>Declaration Selection</label>
-          <select className='ui fluid dropdown'
-            onChange={this.handleDeclarationSelectionChange.bind(this)}
-            value={this.state.fqn}>
-            {this.state.types.map((type)=>{
-              const fqn = type.getFullyQualifiedName();
-              return <option key={fqn} value={fqn}>{fqn}</option>;
-            })}
-          </select>
-        </div>
-        <div className="ui segment">
-          <h2>Form</h2>
-          <div >
-            {form}
-          </div>
-        </div>
-        <div className="ui segment">
-          <h2>JSON</h2>
-          <div className='ui form field'>
-            <textarea
-              value={JSON.stringify(this.state.json, null, 2)}
-              onChange={this.handleJsonTextAreaChange.bind(this)}
-            />
-          </div>
-        </div>
+        <Grid columns={2} divided>
+          <Grid.Row stretched>
+            <Grid.Column>
+              <Segment>
+                <Form>
+                  <Form.Field>
+                    <label>Model</label>
+                    <TextareaAutosize
+                      style={{ whiteSpace: 'pre' }}
+                      value={this.state.model}
+                      onChange={this.handleModelTextAreaChange.bind(this)}/>
+                  </Form.Field>
+                </Form>
+              </Segment>
+              <Segment>
+                <Form>
+                  <Form.Field>
+                    <label>JSON</label>
+                    <TextareaAutosize
+                      style={{ whiteSpace: 'pre' }}
+                      value={JSON.stringify(this.state.json, null, 2)}
+                      onChange={this.handleJsonTextAreaChange.bind(this)}/>
+                  </Form.Field>
+                </Form>
+              </Segment>
+            </Grid.Column>
+            <Grid.Column>
+              <p>Choose a type from this dropdown to generate a form</p>
+              <Form.Field>
+                <label>Declaration Selection</label>
+                <Dropdown
+                  fluid
+                  selection
+                  onChange={this.handleDeclarationSelectionChange.bind(this)}
+                  value={this.state.fqn}
+                  options={this.state.types.map((type)=>{
+                    const fqn = type.getFullyQualifiedName();
+                    return { key: fqn, text: fqn, value: fqn };
+                  })}/>
+              </Form.Field>
+              <Segment>
+                <h2>Form</h2>
+                {form}
+              </Segment>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
       </div>
     );
   }
